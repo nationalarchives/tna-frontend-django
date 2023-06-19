92c38dd2b627fcd4faf5fa1166f2be872953761a
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-06-19 19:24:47 +0100

    Update fixtures directory to non-local

--- tasks/test.mjs
+++ tasks/test.mjs
@@ -1,16 +1,10 @@
-// xidel -se 'request-combine("http://127.0.0.1:8000/piwik.php",{"action_name":"Example",...})/url'
-// # curl "http://127.0.0.1:8080/components/card?title=CUSTOM%20TITLE&text=Lorem%20ipsum" > card.txt
-// curl "http://127.0.0.1:8080/components/card?title=CUSTOM%20TITLE&text=Lorem%20ipsum"
-// # cat /Users/andrew/git/tna-frontend/src/nationalarchives/components/card/fixtures.json
-// cat jq .fixtures /Users/andrew/git/tna-frontend/src/nationalarchives/components/card/fixtures.json
-
 import fetch from "node-fetch";
 import fs from "fs";
 import { diffChars } from "diff";
 import { globSync } from "glob";
 import { html_beautify } from "js-beautify/js/lib/beautify-html.js";
 
-const fixturesDirectory = `/Users/andrew/git/tna-frontend/src/nationalarchives/components/`;
+const fixturesDirectory = `tna-frontend/src/nationalarchives/components/`;
 
 const components = globSync(`${fixturesDirectory}*/fixtures.json`)
   .map((componentFixtureFile) =>
