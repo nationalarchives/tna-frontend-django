e483a85430b28527911a26679e2d76a5400c151e
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-08-18 17:38:04 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nationalarchives-frontend-django",
-    version="0.1.0-alpha",
+    version="0.1.1-alpha",
     author="Andrew Hosgood",
     author_email="andrew.hosgood@nationalarchives.gov.uk",
     description="The National Archives frontend Django templates",
